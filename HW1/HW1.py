de0d518242d1655b6edd5dd8afc5489fd4411cd1
UHManuel <mfcasti3@cougarnet.uh.edu>
2020-09-11 12:52:09 -0500

    Sample

---
+++ HW1/HW1.py
@@ -0,0 +1,14 @@
+print('Birthday Calculator')
+print('Current day')
+
+current_month = int(input('Month: '))
+current_day = int(input('Day: '))
+current_year = int(input('Year: '))
+
+print('Birthday')
+birth_month = int(input('Month: '))
+birth_day = int(input('Day: '))
+birth_year = int(input('Year: '))
+
+years_1 = current_year - birth_year
+print('You are', years_1, 'years old.')
