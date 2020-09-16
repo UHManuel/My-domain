7166c9e3beb0bd0cdbc0a7075b2560e00e5d3d0c
UHManuel <70231911+UHManuel@users.noreply.github.com>
2020-09-16 12:11:30 -0500

    Add files via upload

---
+++ 1.20 lab.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  9 13:14:56 2020
+Manuel Castillo
+CIS 2348
+lab1.20
+@author: 18327
+"""
+
+number_1 = int(input('Enter interger: '))
+
+print('You entered' , number_1)
+
+square_1 = number_1 * number_1
+
+
+cube_1 = number_1 * number_1 * number_1
+
+print(number_1, 'squared is', square_1)
+
+print('And', number_1, "cubed is", cube_1 , '!!')
+
+math_1 = int(input('Enter another interger: '))
+
+add_1 = 4 + math_1
+
+multi_1 = 4 * math_1
+
+print('4 + 5 is', add_1)
+
+print('4 * 5 is', multi_1)
+
