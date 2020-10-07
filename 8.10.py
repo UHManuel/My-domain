268a9a887062f340eb5e268f3aa3f9e256bf4413
UHManuel <70231911+UHManuel@users.noreply.github.com>
2020-10-07 10:25:58 -0500

    Add files via upload

---
+++ 8.10.py
@@ -0,0 +1,16 @@
+"""
+
+Manuel Castillo
+1431705
+
+"""
+
+
+Pali = input()
+# the word in reverse is -1...
+if ( Pali ==  Pali[::-1]):
+    print(Pali, 'is a palindrome' )
+else:
+    print( Pali, 'is not a palindroe')
+
+
